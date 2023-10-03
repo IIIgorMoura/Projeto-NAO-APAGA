a95f4abf665c9d5994fc0ffcd699e8bb87c43d62
IIIgorMoura <igor2007moura@gmail.com>
2023-10-03 15:02:17 -0300

    a

--- Tarefa/main.js
+++ Tarefa/main.js
@@ -23,5 +23,32 @@ function listarTarefas() {
 
         //def txt da tarefa no span
         span.innerHTML = todo;
+
+        //def txt do botao de excluir matriz e mostrar lista dnv
+        button.addEventListener = 'Excluir';
+        addEventListener('click', function(){
+            todos.splice(i, 1);
+            listarTarefas();
+        });
+
+        //add o span e o botao à lista
+        li.appendChild(span);
+        li.appendChild(button);
+
+        //add element li na lista tarefas
+        lista.appendChild(span);
+    };
+};
+
+//detect qnd user envia nova tarefa
+form.addEventListener('submit', function(event) {
+    event.preventDefault;
+
+    //obtem txt e remove espacos brancos desnecessários
+    const texto = input.ariaValueMax.trim();
+
+    //retorna nova tarefa se n tiver txt
+    if(texto.length === 0){
+        return;
     }
-}
+})
